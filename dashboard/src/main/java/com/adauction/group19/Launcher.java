99000241918a55c7bc62d417aaff8163f3394d3a
cp2n23 <cp2n23@soton.ac.uk>
2025-03-04 10:10:54 +0000

    jar file through Launcher class

---
+++ dashboard/src/main/java/com/adauction/group19/Launcher.java
@@ -0,0 +1,11 @@
+package com.adauction.group19;
+
+import com.sun.tools.javac.Main;
+
+public class Launcher {
+  public static void main(String[] args){
+    // Launch the JavaFX application.
+    App.main(args);
+  }
+
+}
