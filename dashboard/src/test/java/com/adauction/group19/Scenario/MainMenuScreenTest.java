5b0bdd95ed6a41fe32d8835ea7d23a166cd7ffa4
Daniel Hoang <hoang.daniel2005@gmail.com>
2025-03-19 22:17:37 +0000

    main menu screen tests

--- dashboard/src/test/java/com/adauction/group19/Scenario/MainMenuScreenTest.java
+++ dashboard/src/test/java/com/adauction/group19/Scenario/MainMenuScreenTest.java
@@ -1,22 +1,80 @@
 package com.adauction.group19.Scenario;
 
+import com.adauction.group19.App;
+import com.adauction.group19.model.CampaignData;
+import com.adauction.group19.service.CampaignDataStore;
 import com.adauction.group19.view.MainMenuScreen;
+import javafx.application.Platform;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
+
+import java.util.List;
+
+import static com.adauction.group19.Util.CampaignDataUtil.createTestCampaignData;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.testfx.api.FxAssert.verifyThat;
+
 
 public class MainMenuScreenTest extends ApplicationTest {
 
     private Stage stage;
+    private CampaignData testData = createTestCampaignData(14);
 
     @Override
     public void start(Stage stage) {
         this.stage = stage;
-        Scene scene = MainMenuScreen.getScene(stage);
-        stage.setScene(scene);
-        stage.show();
+
+        new App().start(stage);
         stage.toFront();
+        stage.requestFocus();
+
+        CampaignDataStore.getInstance().setCampaignData(testData);
     }
 
+    @Test
+    public void testViewInput() {
+        clickOn("#btnInputData");
+    }
 
+    @Test
+    public void testViewMetricsScreenBtn() {
+        clickOn("#btnViewMetrics");
+    }
+
+    @Test
+    public void testViewClickCost() {
+        clickOn("#btnClickCost");
+    }
+
+    @Test
+    public void testToggleTheme() {
+        Parent root = stage.getScene().getRoot();
+
+        List<String> sceneStylesheets = root.getStylesheets();
+
+        System.out.println(sceneStylesheets);
+
+        boolean containsStylesheet = sceneStylesheets.stream().anyMatch(s -> s.endsWith("main.css"))
+                && sceneStylesheets.stream().anyMatch(s -> s.endsWith("dark.css"));
+
+        assertTrue(containsStylesheet, "Scene should have both main.css and dark.css stylesheets applied");
+
+        clickOn("#toggleThemeButton");
+
+        containsStylesheet = sceneStylesheets.stream().anyMatch(s -> s.endsWith("main.css"))
+                && sceneStylesheets.stream().anyMatch(s -> s.endsWith("light.css"));
+
+        assertTrue(containsStylesheet, "Scene should have both main.css and light.css stylesheets applied");
+    }
+
+    @AfterAll
+    static void tearDown() {
+        // Force JavaFX to exit after the tests
+        Platform.exit();
+    }
 }
